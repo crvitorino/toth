40f1d36d3b5918e226dac5a75a864858482a22de
fdettoni <fdettoni@8c964a43-e519-0410-a7e0-67da8b188ab1>
2006-08-01 21:39:18 +0000

    comitando

---
+++ trunk/src/app/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Main.java
+ *
+ * Criado em 1 de Agosto de 2006, 18:23
+ *
+ */
+package app;
+import gui.*;
+/**
+ *
+ * @author Fernando Dettoni
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public static void main(String args[]) {
+        Splash splash = new Splash();
+        splash.setVisible(true);
+    }
+    
+}
