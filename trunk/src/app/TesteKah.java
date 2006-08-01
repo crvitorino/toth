a97d6b2d1eb47639f7d1930ee748d2433e1dd914
kfasolin <kfasolin@8c964a43-e519-0410-a7e0-67da8b188ab1>
2006-08-01 00:21:39 +0000

    inserçao teste

---
+++ trunk/src/app/TesteKah.java
@@ -0,0 +1,22 @@
+/*
+ * TesteKah.java
+ *
+ * Created on 31 de Julho de 2006, 21:18
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package app;
+
+/**
+ *
+ * @author Karina
+ */
+public class TesteKah {
+    
+    /** Creates a new instance of TesteKah */
+    public TesteKah() {
+    }
+    
+}
