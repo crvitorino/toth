77ab81bf45b785138a083130f09383550052c8fc
kfasolin <kfasolin@8c964a43-e519-0410-a7e0-67da8b188ab1>
2006-08-16 01:26:52 +0000

    pronto

--- trunk/src/empresa/Fornecedor.java
+++ trunk/src/empresa/Fornecedor.java
@@ -19,7 +19,7 @@ import java.sql.Statement;
 public class Fornecedor {
     public int codigo;
     public boolean ativo;
-    public String nome, data, fantasia, cnpj, inscest, endereco, bairro, numero, municipio, estado, cep, fone1, fone2, email; 
+    private String nome, data, fantasia, cnpj, inscest, endereco, bairro, numero, municipio, estado, cep, fone1, fone2, email; 
     private ConSQL con;
 
     /**
@@ -102,7 +102,118 @@ public class Fornecedor {
     }
     public Fornecedor(ConSQL con) {
         this.con = con;
-    }    
+    } 
     
+    
+     public boolean apagaFornecedor() {
+        String sql = "delete from fornecedores where id = "+codigo;
+        Statement stmt = con.getStatement();
+        try {
+            stmt.executeUpdate(sql);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+        
+    }
+    public void update() throws SQLException{
+        String sql = "update fornecedores set nome='"+nome+"',ativo ="+ativo+",fantasia='"+fantasia+"',cnpj='"+cnpj+"',inscest='"+inscest+"',endereco='"+
+                        endereco+"',bairro='"+bairro+"',numero='"+numero+"',cidade='"+municipio+"',estado='"+estado+"',cep='"+cep+
+                        "',telefone='"+fone1+"',telefone2='"+fone2+"',email='"+email+"' where id ="+codigo;
+        Statement stmt = con.getStatement();
+        try {
+            stmt.executeUpdate(sql);
+            }
+        catch (SQLException e) {
+	    e.printStackTrace();
+	}
+    }
+        
+      public void setNome(String Nome){
+        this.nome = Nome;
+    }
+    public void setData(String Data){
+        this.data = Data;
+    }
+    public void setFantasia(String Fantasia){
+        this.fantasia = Fantasia;
+    }
+    public void setCnpj(String Cnpj){
+        this.cnpj = Cnpj;
+    }
+    public void setInscest(String Inscest){
+        this.inscest = Inscest;
+    }
+    public void setEndereco(String Endereco){
+        this.endereco = Endereco;
+    }
+    public void setBairro(String Bairro){
+        this.bairro = Bairro;
+    }
+    public void setNumero(String Numero){
+        this.numero = Numero;
+    }
+    public void setMunicipio(String Municipio){
+        this.municipio = Municipio;
+    }
+    public void setEstado(String Estado){
+        this.estado = Estado;
+    }
+    public void setCep(String Cep){
+        this.cep = Cep;
+    }
+    public void setFone1(String Fone1){
+        this.fone1 = Fone1;
+    }
+    public void setFone2(String Fone2){
+        this.fone2 = Fone2;
+    }
+    public void setEmail(String Email){
+        this.email = Email;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getData(){
+        return data;
+    }
+    public String getFantasia(){
+        return fantasia;
+    }
+    public String getCnpj(){
+        return cnpj;
+    }
+    public String getInscest(){
+        return inscest;
+    }
+    public String getEndereco(){
+        return endereco;
+    }
+    public String getBairro(){
+        return bairro;
+    }
+    public String getNumero(){
+        return numero;
+    }
+    public String getMunicipio(){
+        return municipio;
+    }
+    public String getEstado(){
+        return estado;
+    }
+    public String getCep(){
+        return cep;
+    }
+    public String getFone1(){
+        return fone1;
+    }
+    public String getFone2(){
+        return fone2;
+    }
+    public String getEmail(){
+        return email;
+    }  
     
 }
